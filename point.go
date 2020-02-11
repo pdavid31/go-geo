6e4ec95fb89aee43ed5e4cbad551107ffb65b738
Paul David <lg14044@hs-nb.de>
2020-02-11 19:34:25 +0100

    Modified Point

--- point.go
+++ point.go
@@ -3,13 +3,15 @@ package geo
 import "math"
 
 type Point struct {
-	Lat float64
-	Lon float64
+	x float64
+	y float64
+	z float64
+	m float64
 }
 
 // NewPoint creates a new Point object
-func NewPoint(lat float64, lon float64) *Point {
-	return &Point{Lat: lat, Lon: lon}
+func NewPoint(x float64, y float64) *Point {
+	return &Point{x: x, y: y}
 }
 
 // GeometryType returns subtype of geometry
@@ -37,14 +39,13 @@ func (p *Point) AsText() string {
 
 // IsEmpty returns true if the geometric object is an empty Geometry
 func (p *Point) IsEmpty() bool {
+	// TODO: implement
 	return false
 }
 
-// Distance calculates the distance between p and p2
-func (p *Point) Distance(p2 *Point) float64 {
-	distance := math.Sqrt(math.Pow(p.Lat-p2.Lat, 2) + math.Pow(p.Lon-p2.Lon, 2))
-
-	return distance
+func (p *Point) Is3D() bool {
+	// TODO: implement
+	return false
 }
 
 func (p *Point) Equals(another Geometry) bool {
@@ -73,6 +74,13 @@ func (p *Point) Contains(another Geometry) bool {
 
 func (p *Point) Overlaps(another Geometry) bool {
 	panic("implement me")
+}
+
+// Distance calculates the distance between p and p2
+func (p *Point) Distance(p2 *Point) float64 {
+	distance := math.Sqrt(math.Pow(p.x-p2.x, 2) + math.Pow(p.y-p2.y, 2))
+
+	return distance
 }
 
 func (p *Point) Buffer(distance float64) Geometry {
