bb5ed6736bb151a0816d7be420f1723dbc28829b
Paul David <lg14044@hs-nb.de>
2020-02-11 21:40:49 +0100

    Added Multipoints tests

---
+++ multipoint_test.go
@@ -0,0 +1,22 @@
+package geo
+
+import (
+	"testing"
+)
+
+var mp = Multipoint{
+	*p,
+	*p2,
+}
+
+func TestMultipoint_GeometryN(t *testing.T) {
+	if mp.GeometryN(0) == p {
+		t.Error("Multipoint GeometryN failed")
+	}
+}
+
+func TestMultipoint_NumGeometries(t *testing.T) {
+	if len(mp) != 2 {
+		t.Error("Multipoint NumGeometries failed")
+	}
+}
