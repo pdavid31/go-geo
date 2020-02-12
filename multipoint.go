d9a14f62499ceaa327bb8e14a80af756433b4bc4
Paul David <lg14044@hs-nb.de>
2020-02-12 14:04:27 +0100

    Implemented Geometry interface in Multipoint

--- multipoint.go
+++ multipoint.go
@@ -1,7 +1,13 @@
 package geo
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Multipoint []Point
 
+/* CONSTRUCTOR */
 func NewMultipoint(points ...*Point) Multipoint {
 	mp := Multipoint{}
 
@@ -12,8 +18,111 @@ func NewMultipoint(points ...*Point) Multipoint {
 	return mp
 }
 
-// TODO: implement Geometry interface
+/* GEOMETRY */
+func (m Multipoint) GeometryType() string {
+	return "Multipoint"
+}
 
+func (m Multipoint) SRID() int {
+	return 4326
+}
+
+func (m Multipoint) Envelope() Geometry {
+	panic("implement me")
+}
+
+func (m Multipoint) AsText() string {
+	is3D := m.Is3D()
+	rep := strings.ToUpper(m.GeometryType()) + " ("
+	for _, p := range m {
+		x := fmt.Sprintf("%f", p.x)
+		y := fmt.Sprintf("%f", p.y)
+
+		rep += "(" + x + " " + y
+
+		if is3D {
+			z := fmt.Sprintf("%f", p.z)
+			rep += " " + z
+		}
+
+		rep += ")"
+	}
+
+	rep += ")"
+	return rep
+}
+
+func (m Multipoint) IsEmpty() bool {
+	return len(m) == 0
+}
+
+func (m Multipoint) Is3D() bool {
+	for _, p := range m {
+		if !p.Is3D() {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (m Multipoint) Equals(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Disjoint(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Intersects(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Touches(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Crosses(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Within(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Contains(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Overlaps(another Geometry) bool {
+	panic("implement me")
+}
+
+func (m Multipoint) Distance(another Geometry) float64 {
+	panic("implement me")
+}
+
+func (m Multipoint) Buffer(distance float64) Geometry {
+	panic("implement me")
+}
+
+func (m Multipoint) ConvexHull() Geometry {
+	panic("implement me")
+}
+
+func (m Multipoint) Intersection(another Geometry) Geometry {
+	panic("implement me")
+}
+
+func (m Multipoint) Union(another Geometry) Geometry {
+	panic("implement me")
+}
+
+func (m Multipoint) Difference(another Geometry) Geometry {
+	panic("implement me")
+}
+
+/* GEOMETRYCOLLECTION */
 func (m Multipoint) NumGeometries() int {
 	return len(m)
 }
