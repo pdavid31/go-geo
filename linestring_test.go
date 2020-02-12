5a6d51ed627adffb86e611e2ba5cb4ac64243121
Paul David <lg14044@hs-nb.de>
2020-02-12 11:29:54 +0100

    Added additional Linestring tests

--- linestring_test.go
+++ linestring_test.go
@@ -1,6 +1,7 @@
 package geo
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -15,6 +16,25 @@ func TestLinestring_NumGeometries(t *testing.T) {
 
 func TestLinestring_GeometryN(t *testing.T) {
 	if !reflect.DeepEqual(ls.GeometryN(0), p) {
-		t.Error("LineString GeometryN failed")
+		t.Error("Linestring GeometryN failed")
+	}
+}
+
+func TestLinestring_Length(t *testing.T) {
+	correctLength := math.Sqrt(offset * 2)
+	if ls.Length() != correctLength {
+		t.Error("Linestring Length failed")
+	}
+}
+
+func TestLinestring_StartPoint(t *testing.T) {
+	if !reflect.DeepEqual(ls.StartPoint(), p) {
+		t.Error("Linestring StartPoint failed")
+	}
+}
+
+func TestLinestring_EndPoint(t *testing.T) {
+	if !reflect.DeepEqual(ls.EndPoint(), p2) {
+		t.Error("Linestring StartPoint failed")
 	}
 }
