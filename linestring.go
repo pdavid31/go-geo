3d1abdcefd70632f7cfb141b5e7526aac419ea20
Paul David <lg14044@hs-nb.de>
2020-02-12 14:37:03 +0100

    Implemented Geometry interface in Linestring

--- linestring.go
+++ linestring.go
@@ -1,6 +1,10 @@
 package geo
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 type Linestring []Point
 
@@ -16,7 +20,111 @@ func NewLinestring(points ...*Point) Linestring {
 }
 
 /* GEOMETRY */
-// TODO: implement Geometry interface
+func (l Linestring) GeometryType() string {
+	return "Linestring"
+}
+
+func (l Linestring) SRID() int {
+	return 4326
+}
+
+func (l Linestring) Envelope() Geometry {
+	panic("implement me")
+}
+
+func (l Linestring) AsText() string {
+	is3D := l.Is3D()
+	lastIndex := len(l) - 2
+	rep := strings.ToUpper(l.GeometryType()) + " ("
+	for i, p := range l {
+		x := fmt.Sprintf("%f", p.x)
+		y := fmt.Sprintf("%f", p.y)
+
+		rep += x + " " + y
+
+		if is3D {
+			z := fmt.Sprintf("%f", p.z)
+			rep += " " + z
+		}
+
+		if i <= lastIndex {
+			rep += ", "
+		}
+	}
+
+	rep += ")"
+	return rep
+}
+
+func (l Linestring) IsEmpty() bool {
+	return len(l) == 0
+}
+
+func (l Linestring) Is3D() bool {
+	for _, p := range l {
+		if !p.Is3D() {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (l Linestring) Equals(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Disjoint(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Intersects(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Touches(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Crosses(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Within(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Contains(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Overlaps(another Geometry) bool {
+	panic("implement me")
+}
+
+func (l Linestring) Distance(another Geometry) float64 {
+	panic("implement me")
+}
+
+func (l Linestring) Buffer(distance float64) Geometry {
+	panic("implement me")
+}
+
+func (l Linestring) ConvexHull() Geometry {
+	panic("implement me")
+}
+
+func (l Linestring) Intersection(another Geometry) Geometry {
+	panic("implement me")
+}
+
+func (l Linestring) Union(another Geometry) Geometry {
+	panic("implement me")
+}
+
+func (l Linestring) Difference(another Geometry) Geometry {
+	panic("implement me")
+}
 
 /* CURVE */
 func (l Linestring) Length() float64 {
--- linestring_test.go
+++ linestring_test.go
@@ -1,12 +1,43 @@
 package geo
 
 import (
+	"fmt"
 	"math"
 	"reflect"
 	"testing"
 )
 
 var ls = NewLinestring(p, p2)
+
+func TestLinestring_GeometryType(t *testing.T) {
+	if ls.GeometryType() != "Linestring" {
+		t.Error("Linestring GeometryType failed")
+	}
+}
+
+func TestLinestring_SRID(t *testing.T) {
+	if ls.SRID() != 4326 {
+		t.Error("Linestring SRID failed")
+	}
+}
+
+func TestLinestring_AsText(t *testing.T) {
+	if ls.AsText() != fmt.Sprintf("LINESTRING (%f %f %f, %f %f %f)", p.x, p.y, p.z, p2.x, p2.y, p2.z) {
+		t.Error("Linestring AsText failed")
+	}
+}
+
+func TestLinestring_IsEmpty(t *testing.T) {
+	if ls.IsEmpty() {
+		t.Error("Linestring IsEmpty failed")
+	}
+}
+
+func TestLinestring_Is3D(t *testing.T) {
+	if !ls.Is3D() {
+		t.Error("Linestring Is3D failed")
+	}
+}
 
 func TestLinestring_Length(t *testing.T) {
 	correctLength := math.Sqrt(offset * 2)
