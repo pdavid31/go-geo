a251d172cc292769266650eeb33d45b3be278e03
Paul David <lg14044@hs-nb.de>
2020-02-12 11:21:49 +0100

    Corrected Multipoint tests

--- multipoint_test.go
+++ multipoint_test.go
@@ -1,19 +1,20 @@
 package geo
 
 import (
+	"reflect"
 	"testing"
 )
 
 var mp = NewMultipoint(p, p2)
 
-func TestMultipoint_GeometryN(t *testing.T) {
-	if mp.GeometryN(0) == p {
-		t.Error("Multipoint GeometryN failed")
-	}
-}
-
 func TestMultipoint_NumGeometries(t *testing.T) {
 	if len(mp) != 2 {
 		t.Error("Multipoint NumGeometries failex^d")
+	}
+}
+
+func TestMultipoint_GeometryN(t *testing.T) {
+	if !reflect.DeepEqual(mp.GeometryN(0), p) {
+		t.Error("Multipoint GeometryN failed")
 	}
 }
