a63768f00c1043c9d55cc888e7a7c42c9b65f595
Paul David <lg14044@hs-nb.de>
2020-02-13 13:20:22 +0100

    Added Surface interface

---
+++ surface.go
@@ -0,0 +1,10 @@
+package geo
+
+type Surface interface {
+	// Returns the area of this Surface, as measured in the spatial reference system of this Surface
+	Area() float64
+	// Returns the mathematical centroid for this Surface as a Point
+	Centroid() Point
+	// Returns A Point guaranteed to be on this Surface
+	PointOnSurface() Point
+}
