7f7fa437344cd27f085b171568d36feb8e45b835
Paul David <lg14044@hs-nb.de>
2020-02-13 10:38:23 +0100

    Corrected MultiCurve interface

--- multicurve.go
+++ multicurve.go
@@ -4,5 +4,5 @@ type MultiCurve interface {
 	// Returns the Length of this MultiCurve which is equal to the sum of the lengths of the element Curves
 	Length() float64
 	// if this MultiCurve is closed [StartPoint ( ) = EndPoint ( ) for each Curve in this MultiCurve]
-	IsClosed()
+	IsClosed() bool
 }
