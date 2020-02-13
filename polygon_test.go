f85d2d8421b4dbbe50dba77bb0be7569c910d1bb
Paul David <lg14044@hs-nb.de>
2020-02-13 13:37:46 +0100

    Added Polygon tests

---
+++ polygon_test.go
@@ -0,0 +1,31 @@
+package geo
+
+import (
+	"testing"
+)
+
+var poly = NewPolygon(lr)
+
+func TestPolygon_GeometryType(t *testing.T) {
+	if poly.GeometryType() != "Polygon" {
+		t.Error("Polygon GeometryType failed")
+	}
+}
+
+func TestPolygon_SRID(t *testing.T) {
+	if poly.SRID() != 4326 {
+		t.Error("Polygon SRID failed")
+	}
+}
+
+func TestPolygon_IsEmpty(t *testing.T) {
+	if poly.IsEmpty() {
+		t.Error("Polygon IsEmpty failed")
+	}
+}
+
+func TestPolygon_Is3D(t *testing.T) {
+	if !poly.Is3D() {
+		t.Error("Polygon Is3D failed")
+	}
+}
