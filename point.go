16bac7747a881d71984c3c440d92dba0a24f3682
Paul David <lg14044@hs-nb.de>
2020-02-13 10:20:57 +0100

    Changed pointer to value receivers in Point

--- point.go
+++ point.go
@@ -35,33 +35,33 @@ func NewPoint(params ...float64) Point {
 }
 
 /* GEOMETRY */
-func (p *Point) Lat() float64 {
+func (p Point) Lat() float64 {
 	return p.x
 }
 
-func (p *Point) Lon() float64 {
+func (p Point) Lon() float64 {
 	return p.y
 }
 
-func (p *Point) Z() float64 {
+func (p Point) Z() float64 {
 	return p.z
 }
 
-func (p *Point) GeometryType() string {
+func (p Point) GeometryType() string {
 	return "Point"
 }
 
-func (p *Point) SRID() int {
+func (p Point) SRID() int {
 	// TODO: implement
 	return 4326
 }
 
-func (p *Point) Envelope() Geometry {
+func (p Point) Envelope() Geometry {
 	// TODO: implement
 	return nil
 }
 
-func (p *Point) AsText() string {
+func (p Point) AsText() string {
 	// TODO: implement
 	// example POINT ZM (1 1 5 60)
 	rep := strings.ToUpper(p.GeometryType()) + " "
@@ -99,68 +99,68 @@ func (p *Point) AsText() string {
 	return rep
 }
 
-func (p *Point) IsEmpty() bool {
+func (p Point) IsEmpty() bool {
 	// TODO: implement
 	return false
 }
 
-func (p *Point) Is3D() bool {
+func (p Point) Is3D() bool {
 	return p.z != NullValue
 }
 
-func (p *Point) Equals(another Geometry) bool {
+func (p Point) Equals(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Disjoint(another Geometry) bool {
+func (p Point) Disjoint(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Intersects(another Geometry) bool {
+func (p Point) Intersects(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Touches(another Geometry) bool {
+func (p Point) Touches(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Crosses(another Geometry) bool {
+func (p Point) Crosses(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Within(another Geometry) bool {
+func (p Point) Within(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Contains(another Geometry) bool {
+func (p Point) Contains(another Geometry) bool {
 	panic("implement me")
 }
 
-func (p *Point) Overlaps(another Geometry) bool {
+func (p Point) Overlaps(another Geometry) bool {
 	panic("implement me")
 }
 
 // Distance calculates the distance between p and p2
-func (p *Point) Distance(another Geometry) float64 {
+func (p Point) Distance(another Geometry) float64 {
 	panic("implement me")
 }
 
-func (p *Point) Buffer(distance float64) Geometry {
+func (p Point) Buffer(distance float64) Geometry {
 	panic("implement me")
 }
 
-func (p *Point) ConvexHull() Geometry {
+func (p Point) ConvexHull() Geometry {
 	panic("implement me")
 }
 
-func (p *Point) Intersection(another Geometry) Geometry {
+func (p Point) Intersection(another Geometry) Geometry {
 	panic("implement me")
 }
 
-func (p *Point) Union(another Geometry) Geometry {
+func (p Point) Union(another Geometry) Geometry {
 	panic("implement me")
 }
 
-func (p *Point) Difference(another Geometry) Geometry {
+func (p Point) Difference(another Geometry) Geometry {
 	panic("implement me")
 }
